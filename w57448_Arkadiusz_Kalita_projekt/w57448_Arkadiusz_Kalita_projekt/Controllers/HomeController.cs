b188ee38730b6867b9643bfbc0723f10343f03b1
Paweł Filipek <pfman@o2.pl>
2019-07-01 21:00:23 +0200

    Utworzenie cennika

--- w57448_Arkadiusz_Kalita_projekt/w57448_Arkadiusz_Kalita_projekt/Controllers/HomeController.cs
+++ w57448_Arkadiusz_Kalita_projekt/w57448_Arkadiusz_Kalita_projekt/Controllers/HomeController.cs
@@ -38,5 +38,9 @@ namespace w57448_Arkadiusz_Kalita_projekt.Controllers
         {
             return View();
         }
+        public ActionResult Cennik()
+        {
+            return View();
+        }
     }
 }
